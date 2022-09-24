8b61143c6e6886281eea3ceaceb38c0997572de7
akmhmgc <akimuhamaguchi@icloud.com>
2022-09-24 18:58:55 +0900

    配列のキーを指定する

---
+++ 30/main.php
@@ -0,0 +1,13 @@
+<?php
+
+// キーを指定することができる
+$scores = [
+  'first'  => 90, 
+  'second' => 40, 
+  'third'  => 100,
+];
+
+// var_dump($scores);
+print_r($scores);
+
+echo $scores['first'] . PHP_EOL;
