ccc48ad1cdb3f029a996b08dc5875b7519460153
akmhmgc <akimuhamaguchi@icloud.com>
2022-09-25 15:38:37 +0900

    抽象クラス

--- basic_php_objects/16/main.php
+++ basic_php_objects/16/main.php
@@ -3,10 +3,29 @@
 declare(strict_types=1);
 
 abstract class People{
+  protected $name;
+
+  public function __construct($name)
+  {
+    $this->name = $name;
+  }
+
   abstract public function greeting();
 }
 
 class Student extends People{
-
+  public function greeting(){
+    echo "私の名前は $this->name です";
+  }
 }
 
+
+$st = new Student('太郎');
+$st->greeting();
+
+
+function test(People $people){
+  $people->greeting();
+}
+
+test($st);
